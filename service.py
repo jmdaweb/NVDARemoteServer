fb9692bdcb60434be09ed362cb754b7636212693
jmdaweb <jmdaweb@hotmail.com>
2015-05-06 18:41:07 +0200

    The server now can run as a service under Windows

---
+++ service.py
@@ -0,0 +1,20 @@
+import win32serviceutil
+import win32service
+import win32event
+import server
+class NVDARemoteService(win32serviceutil.ServiceFramework):
+	_svc_name_ = "NVDARemoteService"
+	_svc_display_name_ = "NVDARemote relay server"
+	_svc_deps_ = []
+	def __init__(self, args):
+		win32serviceutil.ServiceFramework.__init__(self, args)
+		self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+		self.srv=server.Server(6837)
+
+	def SvcStop(self):
+		self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+		self.srv.close()
+		win32event.SetEvent(self.hWaitStop)
+
+	def SvcDoRun(self):
+		self.srv.run()
--- setup_windows.py
+++ setup_windows.py
@@ -6,12 +6,12 @@ from glob import glob
 def get_data():
  if platform.architecture()[0][:2] == "32":
   return [
-  ("Microsoft.VC90.CRT", glob("msvc32/Microsoft.VC90.CRT/*")),
-  ("Microsoft.VC90.MFC", glob("msvc32/Microsoft.VC90.MFC/*")),]
+  ("Microsoft.VC90.CRT", glob("windows/msvc32/Microsoft.VC90.CRT/*")),
+  ("Microsoft.VC90.MFC", glob("windows/msvc32/Microsoft.VC90.MFC/*")),]
  elif platform.architecture()[0][:2] == "64":
   return [
-  ("Microsoft.VC90.CRT", glob("msvc64/Microsoft.VC90.CRT/*")),
-  ("Microsoft.VC90.MFC", glob("msvc64/Microsoft.VC90.MFC/*")),]
+  ("Microsoft.VC90.CRT", glob("windows/msvc64/Microsoft.VC90.CRT/*")),
+  ("Microsoft.VC90.MFC", glob("windows/msvc64/Microsoft.VC90.MFC/*")),]
 
 if __name__ == '__main__':
  setup(
@@ -20,7 +20,7 @@ if __name__ == '__main__':
   author_email = "jm.delicado@technow.es",
   version = "1.0",
   url = "https://www.technow.es",
-  data_files = get_data()+[("", ["../server.pem"])],
+  data_files = get_data()+[("", ["server.pem"])],
 options = {
    'py2exe': {   
     'optimize':2,
@@ -30,8 +30,14 @@ options = {
   },
   console = [
    {
-    'script': '../server.py',
+    'script': 'server.py',
     'dest_base': 'NVDARemoteServer',
+}
+  ],
+  service = [
+   {
+    'modules': ['service'],
+    'dest_base': 'NVDARemoteService',
 }
   ],
   install_requires = [
