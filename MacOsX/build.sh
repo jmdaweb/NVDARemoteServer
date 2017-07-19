b50ce326d5a6c4fe3d5dfec8ccd0c745f7a972e4
Jose Manuel Delicado <jmdaweb@hotmail.com>
2017-07-19 18:21:03 +0200

    Added -n flag to gzip on mac os build.sh

--- MacOsX/build.sh
+++ MacOsX/build.sh
@@ -5,8 +5,8 @@ mkdir -p package/usr/share/man/man1
 cp ../server.py ../daemon.py ../server.pem package/usr/share/NVDARemoteServer
 cp NVDARemoteServer ../NVDARemoteCertificate package/usr/bin
 cp ../manual/NVDARemoteServer.1 ../manual/NVDARemoteCertificate.1 package/usr/share/man/man1
-gzip -9 package/usr/share/man/man1/NVDARemoteServer.1
-gzip -9 package/usr/share/man/man1/NVDARemoteCertificate.1
+gzip -n -9 package/usr/share/man/man1/NVDARemoteServer.1
+gzip -n -9 package/usr/share/man/man1/NVDARemoteCertificate.1
 chmod +x package/usr/bin/NVDARemoteServer
 chmod +x package/usr/bin/NVDARemoteCertificate
 pkgbuild --identifier NVDARemoteServer --version 1.4.1 --install-location / --root package NVDARemoteServer.pkg
