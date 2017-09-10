930e0af15e3cefe70ff3615031c484bec2f5e7b8
Jose Manuel Delicado <jmdaweb@hotmail.com>
2017-09-10 16:04:06 +0200

    Archlinux: set all binaries as executable with chmod +x before building the package

--- archlinux/build.sh
+++ archlinux/build.sh
@@ -5,6 +5,8 @@ mkdir -p src/usr/share/man/man1
 mkdir pkg
 cp ../server.py ../options.py ../server.pem ../daemon.py src/usr/share/NVDARemoteServer
 cp ../NVDARemoteCertificate src/usr/bin
+chmod +x src/usr/bin/NVDARemoteServer
+chmod +x src/usr/bin/NVDARemoteCertificate
 cp ../NVDARemoteServer.conf src/etc
 cp ../manual/NVDARemoteServer.1 ../manual/NVDARemoteCertificate.1 src/usr/share/man/man1
 makepkg
