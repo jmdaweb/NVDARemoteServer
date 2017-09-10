638a7bda3e0309443a48c4c922dad33f389ce697
Jose Manuel Delicado <jmdaweb@hotmail.com>
2017-09-10 15:29:48 +0200

    Added options.py to arch and MSYS build scripts

--- MSYS2/build.sh
+++ MSYS2/build.sh
@@ -3,7 +3,7 @@ mkdir -p src/usr/share/NVDARemoteServer
 mkdir -p src/usr/share/man/man1
 mkdir -p src/etc
 mkdir pkg
-cp ../server.py ../server.pem ../daemon.py src/usr/share/NVDARemoteServer
+cp ../server.py ../options.py ../server.pem ../daemon.py src/usr/share/NVDARemoteServer
 cp ../NVDARemoteCertificate src/usr/bin
 cp ../manual/NVDARemoteServer.1 ../manual/NVDARemoteCertificate.1 src/usr/share/man/man1
 cp ../NVDARemoteServer.conf src/etc
--- archlinux/build.sh
+++ archlinux/build.sh
@@ -3,7 +3,7 @@ mkdir -p src/etc
 mkdir -p src/usr/share/NVDARemoteServer
 mkdir -p src/usr/share/man/man1
 mkdir pkg
-cp ../server.py ../server.pem ../daemon.py src/usr/share/NVDARemoteServer
+cp ../server.py ../options.py ../server.pem ../daemon.py src/usr/share/NVDARemoteServer
 cp ../NVDARemoteCertificate src/usr/bin
 cp ../NVDARemoteServer.conf src/etc
 cp ../manual/NVDARemoteServer.1 ../manual/NVDARemoteCertificate.1 src/usr/share/man/man1
