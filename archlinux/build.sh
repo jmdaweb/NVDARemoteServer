8bcc9461fce3d916deff3716713db1ce8c4733e3
Jose Manuel Delicado <jmdaweb@hotmail.com>
2018-09-30 17:48:06 +0200

    Start using the same systemd configuration for all platforms, relying on sysusers.d and tmpfiles.d, as they are available even in Centos 7

--- archlinux/build.sh
+++ archlinux/build.sh
@@ -3,6 +3,9 @@ mkdir -p src/etc
 mkdir -p src/usr/share/NVDARemoteServer
 mkdir -p src/usr/share/man/man1
 mkdir -p src/usr/share/man/man5
+mkdir -p src/usr/lib/sysusers.d
+mkdir -p src/usr/lib/tmpfiles.d
+mkdir -p src/usr/lib/systemd/system
 mkdir pkg
 cp ../server.py ../options.py ../server.pem ../daemon.py src/usr/share/NVDARemoteServer
 cp ../NVDARemoteCertificate src/usr/bin
@@ -11,4 +14,7 @@ chmod +x src/usr/bin/NVDARemoteCertificate
 cp ../NVDARemoteServer.conf src/etc
 cp ../manual/NVDARemoteServer.1 ../manual/NVDARemoteCertificate.1 src/usr/share/man/man1
 cp ../manual/NVDARemoteServer.conf.5 src/usr/share/man/man5
+cp ../systemd/NVDARemoteServer.tmpfiles src/usr/lib/tmpfiles.d/NVDARemoteServer.conf
+cp ../systemd/NVDARemoteServer.sysusers src/usr/lib/sysusers.d/NVDARemoteServer.conf
+cp ../systemd/NVDARemoteServer.service src/usr/lib/systemd/system
 makepkg
--- debian8/build.sh
+++ debian8/build.sh
@@ -7,6 +7,9 @@ mkdir -p nvda-remote-server_$VERSION/usr/share/NVDARemoteServer
 mkdir -p nvda-remote-server_$VERSION/usr/share/man/man1
 mkdir -p nvda-remote-server_$VERSION/usr/share/man/man5
 mkdir -p nvda-remote-server_$VERSION/etc
+mkdir -p nvda-remote-server_$VERSION/lib/systemd/system
+mkdir -p nvda-remote-server_$VERSION/usr/lib/sysusers.d
+mkdir -p nvda-remote-server_$VERSION/usr/lib/tmpfiles.d
 #copy files
 cp ../NVDARemoteCertificate nvda-remote-server_$VERSION/usr/bin
 cp ../NVDARemoteServer.conf nvda-remote-server_$VERSION/etc
@@ -15,6 +18,9 @@ cp ../manual/NVDARemoteServer.1 ../manual/NVDARemoteCertificate.1 nvda-remote-se
 cp ../manual/NVDARemoteServer.conf.5 nvda-remote-server_$VERSION/usr/share/man/man5
 cp ../copyright nvda-remote-server_$VERSION/usr/share/doc/nvda-remote-server
 cp ../changelog.Debian nvda-remote-server_$VERSION/usr/share/doc/nvda-remote-server
+cp ../systemd/NVDARemoteServer.service nvda-remote-server_$VERSION/lib/systemd/system
+cp ../systemd/NVDARemoteServer.tmpfiles nvda-remote-server_$VERSION/usr/lib/tmpfiles.d/NVDARemoteServer.conf
+cp ../systemd/NVDARemoteServer.sysusers nvda-remote-server_$VERSION/usr/lib/sysusers.d/NVDARemoteServer.conf
 #compress manual and changelog
 gzip -n -9 nvda-remote-server_$VERSION/usr/share/man/man1/NVDARemoteServer.1
 gzip -n -9 nvda-remote-server_$VERSION/usr/share/man/man5/NVDARemoteServer.conf.5
@@ -35,7 +41,9 @@ chmod -x nvda-remote-server_$VERSION/usr/share/man/man5/NVDARemoteServer.conf.5.
 chmod -x nvda-remote-server_$VERSION/usr/share/man/man1/NVDARemoteCertificate.1.gz
 chmod -x nvda-remote-server_$VERSION/usr/share/doc/nvda-remote-server/changelog.Debian.gz
 chmod -x nvda-remote-server_$VERSION/lib/systemd/system/NVDARemoteServer.service
+chmod -x nvda-remote-server_$VERSION/usr/lib/sysusers.d/NVDARemoteServer.conf
+chmod -x nvda-remote-server_$VERSION/usr/lib/tmpfiles.d/NVDARemoteServer.conf
 chmod -x nvda-remote-server_$VERSION/etc/NVDARemoteServer.conf
-sed -i "s/.*=\/var\/run\/NVDARemoteServer.pid.*/pidfile=\/var\/run\/NVDARemoteServer\/NVDARemoteServer.pid/" nvda-remote-server_$VERSION/etc/NVDARemoteServer.conf
+sed -i "s/.*=\/var\/run\/NVDARemoteServer.pid.*/pidfile=\/run\/NVDARemoteServer\/NVDARemoteServer.pid/" nvda-remote-server_$VERSION/etc/NVDARemoteServer.conf
 #build the package
 dpkg-deb --build nvda-remote-server_$VERSION
