7c942f873996711a955914cfe810e1cb4a2d7b5d
Jose Manuel Delicado <jmdaweb@hotmail.com>
2020-11-28 14:23:16 +0100

    Applied flake8 fixes to pyinstaller specification and cx_freeze script

--- pyinstaller.spec
+++ pyinstaller.spec
@@ -1,38 +1,14 @@
 # -*- mode: python -*-
 import sys
-block_cipher = None
 import platform
-arch=platform.architecture()[0][:2]
-libs=[]
+block_cipher = None
+arch = platform.architecture()[0][:2]
+libs = []
 if sys.version.startswith("3"):
- arch=arch+"_py3"
- libs=[("windows/msvc"+arch+"/*.dll", ".")]
+	arch = arch + "_py3"
+	libs = [("windows/msvc" + arch + "/*.dll", ".")]
 
-a = Analysis(['server.py'],
-             pathex=[],
-             binaries=libs,
-             datas=[("windows/*.cmd", "."), ("windows/msvc"+arch+"/openssl*", "."), ("server.pem", "."), ("NVDARemoteServer.conf", ".")],
-             hiddenimports=["win32timezone"],
-             hookspath=[],
-             runtime_hooks=[],
-             excludes=[],
-             win_no_prefer_redirects=False,
-             win_private_assemblies=False,
-             cipher=block_cipher)
-pyz = PYZ(a.pure, a.zipped_data,
-             cipher=block_cipher)
-exe = EXE(pyz,
-          a.scripts,
-          exclude_binaries=True,
-          name='NVDARemoteServer',
-          debug=False,
-          strip=False,
-          upx=False,
-          console=True )
-coll = COLLECT(exe,
-               a.binaries,
-               a.zipfiles,
-               a.datas,
-               strip=False,
-               upx=False,
-               name='NVDARemoteServer')
+a = Analysis(['server.py'], pathex=[], binaries=libs, datas=[("windows/*.cmd", "."), ("windows/msvc" + arch + "/openssl*", "."), ("server.pem", "."), ("NVDARemoteServer.conf", ".")], hiddenimports=["win32timezone"], hookspath=[], runtime_hooks=[], excludes=[], win_no_prefer_redirects=False, win_private_assemblies=False, cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+exe = EXE(pyz, a.scripts, exclude_binaries=True, name='NVDARemoteServer', debug=False, strip=False, upx=False, console=True)
+coll = COLLECT(exe, a.binaries, a.zipfiles, a.datas, strip=False, upx=False, name='NVDARemoteServer')
--- setup_windows_cxfreeze.py
+++ setup_windows_cxfreeze.py
@@ -4,39 +4,41 @@ import platform
 import sys
 from glob import glob
 
-def get_data():
- data=["server.pem", "windows/service_manager.cmd", "windows/debug.cmd", "windows/NVDARemoteCertificate.cmd", "NVDARemoteServer.conf"]
- if platform.architecture()[0][:2] == "32" and sys.version.startswith("2"):
-  return data+["windows/msvc32/openssl.exe", "windows/msvc32/openssl.cnf"]
- elif platform.architecture()[0][:2] == "64" and sys.version.startswith("2"):
-  return data+["windows/msvc64/openssl.exe", "windows/msvc64/openssl.cnf"]
- elif platform.architecture()[0][:2] == "32" and sys.version.startswith("3"):
-  return data+glob("windows/msvc32_py3/*")
- elif platform.architecture()[0][:2] == "64" and sys.version.startswith("3"):
-  return data+glob("windows/msvc64_py3/*")
 
-build_exe_options={
-  'optimize': 2,
-  'include_msvcr': True,
-  'bin_excludes':['CRYPT32.dll'],
-  'include_files': get_data(),
-  'includes':['win32timezone'],
-  'zip_include_packages':"*",
-  'zip_exclude_packages':[],
+def get_data():
+	data = ["server.pem", "windows/service_manager.cmd", "windows/debug.cmd", "windows/NVDARemoteCertificate.cmd", "NVDARemoteServer.conf"]
+	if platform.architecture()[0][:2] == "32" and sys.version.startswith("2"):
+		return data + ["windows/msvc32/openssl.exe", "windows/msvc32/openssl.cnf"]
+	elif platform.architecture()[0][:2] == "64" and sys.version.startswith("2"):
+		return data + ["windows/msvc64/openssl.exe", "windows/msvc64/openssl.cnf"]
+	elif platform.architecture()[0][:2] == "32" and sys.version.startswith("3"):
+		return data + glob("windows/msvc32_py3/*")
+	elif platform.architecture()[0][:2] == "64" and sys.version.startswith("3"):
+		return data + glob("windows/msvc64_py3/*")
+
+
+build_exe_options = {
+	'optimize': 2,
+	'include_msvcr': True,
+	'bin_excludes': ['CRYPT32.dll'],
+	'include_files': get_data(),
+	'includes': ['win32timezone'],
+	'zip_include_packages': "*",
+	'zip_exclude_packages': [],
 }
 
 if __name__ == '__main__':
- setup(
-  name = "NVDARemote server",
-  author = "Jose Manuel Delicado",
-  author_email = "jm.delicado@nvda.es",
-  version = "2.0",
-  url = "https://github.com/jmdaweb/NVDARemoteServer",
-  description="NVDA Remote Relay Server",
-options = {
-   'build_exe': build_exe_options,
-  },
-  executables = [
-    Executable("server.py", targetName="NVDARemoteServer.exe")
-  ]
- )
+	setup(
+		name="NVDARemote server",
+		author="Jose Manuel Delicado",
+		author_email="jm.delicado@nvda.es",
+		version="2.0",
+		url="https://github.com/jmdaweb/NVDARemoteServer",
+		description="NVDA Remote Relay Server",
+		options={
+			'build_exe': build_exe_options,
+		},
+		executables=[
+			Executable("server.py", targetName="NVDARemoteServer.exe")
+		]
+	)
