5ae2186f07628fc5b213b4a25c18c926618128a2
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2021-10-31 19:57:16 +0100

    NVDARemoteCertificate: use ecdsa instead of rsa to increase encryption strength. This change may be useful in the future

--- windows/NVDARemoteCertificate.cmd
+++ windows/NVDARemoteCertificate.cmd
@@ -3,8 +3,8 @@ title NVDA Remote Server self-signed certificate generator
 cd %~dp0
 setlocal
 set OPENSSL_CONF=%~dp0openssl.cnf
-openssl genrsa -out key.key 4096
-openssl req -new -key key.key -out cert.csr
+openssl genpkey -out key.key -algorithm EC -pkeyopt ec_paramgen_curve:secp384r1
+openssl req -new -key key.key -out cert.csr -sha512
 openssl x509 -req -days 3650 -in cert.csr -signkey key.key -out cert.crt
 del server.pem
 copy key.key + cert.crt server.pem
